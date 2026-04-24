4fe4aab6673056880302eba89fca9b591f68a616
Ismael473 <ismaelmena73@gmail.com>
2026-04-24 17:04:43 -0600

    Primeras palabras del diccionario
    Falta agregar más palabras a cada carrera y sininonimos de las afinidades.

---
+++ Diccionario.pl
@@ -0,0 +1,49 @@
+affinity(medicina, ayuda).
+affinity(medicina, personas).
+affinity(medicina, salud).
+affinity(medicina, biologia).
+affinity(medicina, sangre).
+affinity(medicina, enfermedades).
+affinity(medicina, medicamentos).
+affinity(medicina, empatia).
+affinity(medicina, resiliencia).
+affinity(medicina, anatomia).
+affinity(medicina, altruismo).
+affinity(medicina, memoria_retentiva).
+affinity(medicina, precision).
+affinity(medicina, quimica_organica).
+affinity(medicina, investigacion_clinica).
+affinity(medicina, etica).
+
+affinity(ingenieria_computadores, abstraccion).
+affinity(ingenieria_computadores, logica_matematica
+affinity(ingenieria_computadores, tecnologia).
+affinity(ingenieria_computadores, circuitos).
+affinity(ingenieria_computadores, matematicas).
+affinity(ingenieria_computadores, dinero).
+affinity(ingenieria_computadores, problemas).
+affinity(ingenieria_computadores, ciencia).
+affinity(ingenieria_computadores, investigacion).
+
+affinity(economia, matematicas).
+affinity(economia, estadistica).
+affinity(economia, dinero).
+affinity(economia, inversiones).
+
+affinity(leyes, investigacion).
+affinity(leyes, lectura).
+affinity(leyes, personas).
+affinity(leyes, defensa).
+affinity(leyes, constitucion).
+affinity(leyes, problemas).
+affinity(leyes, 
+
+affinity(bombero, personas).
+affinity(bombero, ayuda).
+affinity(bombero, fuego).
+affinity(bombero, urgencia).
+affinity(bombero, problemas).
+affinity(bombero, condicion_fisica).
+
+
+
